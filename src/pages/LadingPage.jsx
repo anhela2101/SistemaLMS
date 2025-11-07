6d66cc50a0aaeeb774180c63c769f8c3948524b2
Ing. Armando Campos <armandocamposf@gmail.com>
2025-11-07 10:39:30 -0400

    _fix: udpate

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Button from '../components/generals/Button'
+import Input from '../components/generals/Input'
+import { Eye, Mail } from 'lucide-react'
 
 const LadingPage = () => {
   return (
     <>
       <Button variant="primary" label="Iniciar Sesion" />
+      <Input label="Email" placeholder="Email" iconLeft={<Mail />} />
     </>
   )
 }
