933d88ac3951027e9552b6ba18784a9a20fa5fae
Ing. Armando Campos <armandocamposf@gmail.com>
2025-11-07 10:21:14 -0400

    _fix: update

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const LadingPage = () => {
   return (
-    <div>LadingPage</div>
+    <h1 className="text-3xl font-bold text-green-500 underline">
+      Hello world!
+    </h1>
   )
 }
 
